7a6ecf8e208fb8c8f90c38dce30cfd159e296d16
Giovannytj <giovanny.torrez@ucb.edu.bo>
2020-11-30 02:02:21 -0400

    Script de código morse

---
+++ morse.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# buscar el nombre del dispositivo en el nombre de la variable
+
+. /lib/ramips.sh
+$tabla=$(ramips_board_name)
+
+_MorseMain () {
+        echo morse > /sys/class/leds/$tabla\:amber\:system/trigger
+        echo 120 > /sys/class/leds/$tabla\:amber\:system/delay
+        echo $* > /sys/class/leds/$tabla\:amber\:system/message
+}
+_MorseMain $*
+
+exit
